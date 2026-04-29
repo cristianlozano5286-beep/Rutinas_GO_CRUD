2b223291326dd2cc22956b39b92a1f1dc9715bc7
cristialozano5286-beep <cristianlozano5286@gmail.com>
2026-04-29 10:11:45 -0500

    funcion para obtener todas las rutinas

--- controllers/rutinas_controller.go
+++ controllers/rutinas_controller.go
@@ -8,3 +8,38 @@ import (
 
 	"github.com/gorilla/mux"
 )
+// GetAllRutinas obtener todas las rutinas c
+func GetAllRutinas(w http.ResponseWriter, r *http.Request) {
+	query := `SELECT id_usuario, nombre, descripcion, objetivo, activo, fecha_modificacion, fecha_creacion
+	           FROM rutinas WHERE 1=1`
+
+	nombre := r.URL.Query().Get("nombre")
+	objetivo := r.URL.Query().Get("objetivo")
+	usuarioID := r.URL.Query().Get("id_usuario")
+
+	if nombre != "" {
+		query += " AND nombre LIKE '%" + nombre + "%'"
+	}
+	if objetivo != "" {
+		query += " AND objetivo LIKE '%" + objetivo + "%'"
+	}
+	if usuarioID != "" {
+		query += " AND id_usuario = " + usuarioID
+	}
+
+	rows, err := config.DB.Query(query)
+	if err != nil {
+		respondJSON(w, 500, map[string]string{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	var list []models.Rutina
+	for rows.Next() {
+		var ru models.Rutina
+		rows.Scan(&ru.IDUsuario, &ru.Nombre, &ru.Descripcion, &ru.Objetivo, &ru.Activo, &ru.FechaModificacion, &ru.FechaCreacion)
+		list = append(list, ru)
+	}
+
+	respondJSON(w, 200, list)
+}
