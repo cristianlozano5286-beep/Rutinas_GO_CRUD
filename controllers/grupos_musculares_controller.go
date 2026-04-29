f2bc3f0eb6ae3c5985b300b4a760d8d602428ced
cristialozano5286-beep <cristianlozano5286@gmail.com>
2026-04-29 09:38:44 -0500

    funcion get de grupos musculares

--- controllers/grupos_musculares_controller.go
+++ controllers/grupos_musculares_controller.go
@@ -9,3 +9,28 @@ import (
 
 	"github.com/gorilla/mux"
 )
+// GetAllGruposMusculares obtener grupos musculares 
+func GetAllGruposMusculares(w http.ResponseWriter, r *http.Request) {
+	query := "SELECT id, nombre, descripcion, activo, fecha_modificacion, fecha_creacion FROM grupos_musculares WHERE 1=1"
+
+	nombre := r.URL.Query().Get("nombre")
+	if nombre != "" {
+		query += " AND nombre LIKE '%" + nombre + "%'"
+	}
+
+	rows, err := config.DB.Query(query)
+	if err != nil {
+		respondJSON(w, 500, map[string]string{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	var list []models.GrupoMuscular
+	for rows.Next() {
+		var g models.GrupoMuscular
+		rows.Scan(&g.ID, &g.Nombre, &g.Descripcion, &g.Activo, &g.FechaModificacion, &g.FechaCreacion)
+		list = append(list, g)
+	}
+
+	respondJSON(w, 200, list)
+}
