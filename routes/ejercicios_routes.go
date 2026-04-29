5d9bea395fc6cddca6d5da989174410a1e86619d
cristialozano5286-beep <cristianlozano5286@gmail.com>
2026-04-29 10:38:35 -0500

    funcion de las rutas

--- routes/ejercicios_routes.go
+++ routes/ejercicios_routes.go
@@ -4,3 +4,10 @@ import (
 	"api_rutinas_gym/controllers"
 	"github.com/gorilla/mux"
 )
+func RegisterEjerciciosRoutes(r *mux.Router) {
+	r.HandleFunc("/ejercicios", controllers.GetAllEjercicios).Methods("GET")
+	r.HandleFunc("/ejercicios/{id}", controllers.GetEjercicioByID).Methods("GET")
+	r.HandleFunc("/ejercicios", controllers.CreateEjercicio).Methods("POST")
+	r.HandleFunc("/ejercicios/{id}", controllers.UpdateEjercicio).Methods("PUT")
+	r.HandleFunc("/ejercicios/{id}", controllers.DeleteEjercicio).Methods("DELETE")
+}
