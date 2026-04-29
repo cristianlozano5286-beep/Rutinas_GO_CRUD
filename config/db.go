2bd06a977063a4b32d072a2bec8d30b6514fe7c9
cristialozano5286-beep <cristianlozano5286@gmail.com>
2026-04-29 09:16:43 -0500

    se establece la conexion a la base de datos

--- config/db.go
+++ config/db.go
@@ -7,3 +7,31 @@ import (
 
 	_ "github.com/lib/pq"
 )
+// ConnectDB establece conexión con PostgreSQL
+func ConnectDB() {
+	host     := "localhost"
+	port     := 5432
+	user     := "postgres"
+	password := "postgres" // 
+	dbname   := "thehousefit_db" // 
+	schema   := "rutinas"
+
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s search_path=%s sslmode=disable",
+		host, port, user, password, dbname, schema,
+	)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal("Error al abrir conexión: ", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("No se puede conectar a la BD: ", err)
+	}
+
+	fmt.Println("Conexión a base de datos exitosa")
+	fmt.Println("Conectado a la DB:", dbname, "| Esquema:", schema)
+	DB = db
+}
