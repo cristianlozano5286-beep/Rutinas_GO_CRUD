c9f6a1aa9e6eac5946410229a389f94dba312962
cristialozano5286-beep <cristianlozano5286@gmail.com>
2026-04-29 10:06:29 -0500

    funcion para enviar respuesta al json

--- controllers/helpers.go
+++ controllers/helpers.go
@@ -4,3 +4,9 @@ import (
 	"encoding/json"
 	"net/http"
 )
+// respondJSON es un helper para enviar respuestas JSON
+func respondJSON(w http.ResponseWriter, status int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(payload)
+}
