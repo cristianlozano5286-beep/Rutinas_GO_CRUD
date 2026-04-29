836fdd18d4338f370f8d7e4000218cd62c1e1357
cristialozano5286-beep <cristianlozano5286@gmail.com>
2026-04-29 10:53:10 -0500

    se crea la funcion de rutas de rutinas

--- routes/rutinas_routes.go
+++ routes/rutinas_routes.go
@@ -4,3 +4,10 @@ import (
 	"api_rutinas_gym/controllers"
 	"github.com/gorilla/mux"
 )
+func RegisterRutinasRoutes(r *mux.Router) {
+	r.HandleFunc("/rutinas", controllers.GetAllRutinas).Methods("GET")
+	r.HandleFunc("/rutinas/{id_usuario}", controllers.GetRutinaByUsuario).Methods("GET")
+	r.HandleFunc("/rutinas", controllers.CreateRutina).Methods("POST")
+	r.HandleFunc("/rutinas/{id_usuario}", controllers.UpdateRutina).Methods("PUT")
+	r.HandleFunc("/rutinas/{id_usuario}", controllers.DeleteRutina).Methods("DELETE")
+}
