fe4c61a43057ceaab50a265c7f2d0f8e98b251db
cristialozano5286-beep <cristianlozano5286@gmail.com>
2026-04-29 10:43:40 -0500

    se crea la funcion de routes grupos musculares

--- routes/grupos_musculares_routes.go
+++ routes/grupos_musculares_routes.go
@@ -4,3 +4,10 @@ import (
 	"api_rutinas_gym/controllers"
 	"github.com/gorilla/mux"
 )
+func RegisterGruposMuscularesRoutes(r *mux.Router) {
+	r.HandleFunc("/grupos-musculares", controllers.GetAllGruposMusculares).Methods("GET")
+	r.HandleFunc("/grupos-musculares/{id}", controllers.GetGrupoMuscularByID).Methods("GET")
+	r.HandleFunc("/grupos-musculares", controllers.CreateGrupoMuscular).Methods("POST")
+	r.HandleFunc("/grupos-musculares/{id}", controllers.UpdateGrupoMuscular).Methods("PUT")
+	r.HandleFunc("/grupos-musculares/{id}", controllers.DeleteGrupoMuscular).Methods("DELETE")
+}
