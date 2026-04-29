09a0938a92ea8c8d7206822bedbc2571c4b0a740
cristialozano5286-beep <cristianlozano5286@gmail.com>
2026-04-29 09:20:36 -0500

    funcion del get en ejercicios

--- controllers/ejercicios_controller.go
+++ controllers/ejercicios_controller.go
@@ -9,3 +9,43 @@ import (
 
 	"github.com/gorilla/mux"
 )
+// GetAllEjercicios de los ejercicios 
+func GetAllEjercicios(w http.ResponseWriter, r *http.Request) {
+	query := `SELECT id, grupo_muscular_id, nombre, descripcion_corta, descripcion_larga,
+	           posicion_inicial, ejecucion, consejos, nivel, activo, fecha_modificacion, fecha_creacion
+	           FROM ejercicios WHERE 1=1`
+
+	nombre := r.URL.Query().Get("nombre")
+	nivel := r.URL.Query().Get("nivel")
+	grupoID := r.URL.Query().Get("grupo_muscular_id")
+
+	if nombre != "" {
+		query += " AND nombre LIKE '%" + nombre + "%'"
+	}
+	if nivel != "" {
+		query += " AND nivel = '" + nivel + "'"
+	}
+	if grupoID != "" {
+		query += " AND grupo_muscular_id = " + grupoID
+	}
+
+	rows, err := config.DB.Query(query)
+	if err != nil {
+		respondJSON(w, 500, map[string]string{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	var list []models.Ejercicio
+	for rows.Next() {
+		var e models.Ejercicio
+		rows.Scan(
+			&e.ID, &e.GrupoMuscularID, &e.Nombre, &e.DescripcionCorta, &e.DescripcionLarga,
+			&e.PosicionInicial, &e.Ejecucion, &e.Consejos, &e.Nivel, &e.Activo,
+			&e.FechaModificacion, &e.FechaCreacion,
+		)
+		list = append(list, e)
+	}
+
+	respondJSON(w, 200, list)
+}
